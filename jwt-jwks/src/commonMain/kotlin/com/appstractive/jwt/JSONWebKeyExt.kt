63ba675453f46f07336027e48f97ce58ee830217
Andreas Schulz <andr.schulz90@gmail.com>
2024-08-15 13:32:12 +0200

    Missing PSS JWKS Verification

---
+++ jwt-jwks/src/commonMain/kotlin/com/appstractive/jwt/JSONWebKeyExt.kt
@@ -0,0 +1,77 @@
+﻿package com.appstractive.jwt
+
+import dev.whyoleg.cryptography.algorithms.asymmetric.EC
+import dev.whyoleg.cryptography.algorithms.asymmetric.RSA
+import dev.whyoleg.cryptography.algorithms.digest.SHA256
+import dev.whyoleg.cryptography.algorithms.symmetric.HMAC
+import dev.whyoleg.cryptography.operations.signature.SignatureVerifier
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+internal val json: Json = Json {
+  isLenient = true
+  explicitNulls = false
+  encodeDefaults = true
+  ignoreUnknownKeys = true
+}
+
+fun JSONWebKeySet.getKey(kid: String?): JSONWebKey =
+    when {
+      kid == null && keys.size == 1 -> {
+        keys.first()
+      }
+      kid != null -> {
+        keys.first { it.kid == kid }
+      }
+      else -> throw IllegalArgumentException("No valid key found for JWT")
+    }
+
+suspend fun JSONWebKey.getVerifier(serializer: Json = json): SignatureVerifier {
+  val digest = alg?.digest ?: SHA256
+
+  return when (this) {
+    is JSONWebKeyEC ->
+        ecdsa
+            .publicKeyDecoder(curve = crv.curve)
+            .decodeFrom(
+                format = EC.PublicKey.Format.JWK,
+                input = serializer.encodeToString(this).encodeToByteArray(), // TODO NYI in Crypto
+            )
+            .signatureVerifier(digest)
+
+    is JSONWebKeyHMAC ->
+        hmac
+            .keyDecoder(digest)
+            .decodeFrom(
+                format = HMAC.Key.Format.JWK,
+                input = serializer.encodeToString(this).encodeToByteArray(), // TODO NYI in Crypto
+            )
+            .signatureVerifier()
+
+    is JSONWebKeyRSA ->
+        when (alg) {
+          Algorithm.PS256,
+          Algorithm.PS384,
+          Algorithm.PS512 ->
+              pss.publicKeyDecoder(digest)
+                  .decodeFromBlocking(
+                      format = RSA.PublicKey.Format.JWK,
+                      input =
+                          serializer.encodeToString(this).encodeToByteArray(), // TODO NYI in Crypto
+                  )
+                  .signatureVerifier()
+          Algorithm.RS256,
+          Algorithm.RS384,
+          Algorithm.RS512 ->
+              pkcs1
+                  .publicKeyDecoder(digest)
+                  .decodeFromBlocking(
+                      format = RSA.PublicKey.Format.JWK,
+                      input =
+                          serializer.encodeToString(this).encodeToByteArray(), // TODO NYI in Crypto
+                  )
+                  .signatureVerifier()
+          else -> throw IllegalArgumentException("Unknown algorithm ${alg}")
+        }
+  }
+}
--- jwt-jwks/src/commonMain/kotlin/com/appstractive/jwt/Jwks.kt
+++ jwt-jwks/src/commonMain/kotlin/com/appstractive/jwt/Jwks.kt
@@ -2,10 +2,8 @@
 
 import dev.whyoleg.cryptography.CryptographyProvider
 import dev.whyoleg.cryptography.CryptographyProviderApi
-import dev.whyoleg.cryptography.algorithms.asymmetric.EC
 import dev.whyoleg.cryptography.algorithms.asymmetric.ECDSA
 import dev.whyoleg.cryptography.algorithms.asymmetric.RSA
-import dev.whyoleg.cryptography.algorithms.digest.SHA256
 import dev.whyoleg.cryptography.algorithms.symmetric.HMAC
 import dev.whyoleg.cryptography.operations.signature.SignatureVerifier
 import io.ktor.client.*
@@ -18,8 +16,6 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 private val provider by lazy { CryptographyProvider.Default }
 internal val pkcs1: RSA<RSA.PKCS1.PublicKey, RSA.PKCS1.PrivateKey, RSA.PKCS1.KeyPair> by lazy {
@@ -27,51 +23,28 @@ internal val pkcs1: RSA<RSA.PKCS1.PublicKey, RSA.PKCS1.PrivateKey, RSA.PKCS1.Key
 }
 internal val hmac by lazy { provider.get(HMAC) }
 internal val ecdsa: ECDSA by lazy { provider.get(ECDSA) }
+internal val pss: RSA.PSS by lazy { provider.get(RSA.PSS) }
 
 @OptIn(CryptographyProviderApi::class)
-class JwksVerifier(
+internal class JwksVerifier(
     private val config: JwksConfig,
 ) : VerificationAlgorithm {
 
   private var keySet: JSONWebKeySet = JSONWebKeySet(emptyList())
-  private var lastUpdate: Instant = Clock.System.now()
+  private var lastUpdate: Instant = Clock.System.now() - config.cacheDuration
 
   private val keyVerifier: MutableMap<String?, SignatureVerifier> = mutableMapOf()
 
-  private val serializer: Json = Json {
-    isLenient = true
-    explicitNulls = false
-    encodeDefaults = true
-    ignoreUnknownKeys = true
-  }
-
-  private val client =
-      config.client.config {
-        install(ContentNegotiation) {
-          json(serializer)
-        }
-      }
+  private val client = config.client.config { install(ContentNegotiation) { json(json) } }
   private val endpoint = checkNotNull(config.endpoint) { "Endpoint not configured" }
 
   override suspend fun verifier(jwt: JWT): SignatureVerifier {
     updateKeySet()
 
     val kid = jwt.header.kid
-    val key: JSONWebKey =
-        when {
-          kid == null && keySet.keys.size == 1 -> {
-            keySet.keys.first()
-          }
-          kid != null -> {
-            keySet.keys.first { it.kid == kid }
-          }
-          else -> throw IllegalArgumentException("No valid key found for JWT")
-        }
+    val key: JSONWebKey = keySet.getKey(kid)
 
-    val verifier: SignatureVerifier =
-        getVerifier(
-            key = key,
-        )
+    val verifier: SignatureVerifier = keyVerifier.getOrPut(kid) { key.getVerifier() }
 
     return object : SignatureVerifier {
       override fun verifySignatureBlocking(
@@ -80,41 +53,6 @@ class JwksVerifier(
       ): Boolean {
         return verifier.verifySignatureBlocking(
             dataInput = dataInput, signatureInput = signatureInput)
-      }
-    }
-  }
-
-  private suspend fun getVerifier(key: JSONWebKey): SignatureVerifier {
-    val digest = key.alg?.digest ?: SHA256
-
-    return keyVerifier.getOrPut(key.kid) {
-      when (key) {
-        is JSONWebKeyEC ->
-          ecdsa
-              .publicKeyDecoder(curve = key.crv.curve)
-              .decodeFrom(
-                  format = EC.PublicKey.Format.JWK,
-                  input = serializer.encodeToString(key).encodeToByteArray(), // TODO NYI in Crypto
-              )
-              .signatureVerifier(digest)
-
-        is JSONWebKeyHMAC ->
-          hmac
-              .keyDecoder(digest)
-              .decodeFrom(
-                  format = HMAC.Key.Format.JWK,
-                  input = serializer.encodeToString(key).encodeToByteArray(), // TODO NYI in Crypto
-              )
-              .signatureVerifier()
-
-        is JSONWebKeyRSA ->
-          pkcs1
-              .publicKeyDecoder(digest)
-              .decodeFromBlocking(
-                  format = RSA.PublicKey.Format.JWK,
-                  input = serializer.encodeToString(key).encodeToByteArray(), // TODO NYI in Crypto
-              )
-              .signatureVerifier()
       }
     }
   }
@@ -135,4 +73,15 @@ class JwksConfig {
   var endpoint: String? = null
   var client: HttpClient = HttpClient()
   var cacheDuration: Duration = 24.hours
+}
+
+fun Verifier.jwks(configure: JwksConfig.() -> Unit) {
+  val config = JwksConfig().apply(configure)
+  val jwks = JwksVerifier(config = config)
+  Algorithm.entries.forEach {
+    algorithm(
+        type = it,
+        algorithm = jwks,
+    )
+  }
 }
