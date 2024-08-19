fdea8c0e30568efa951bf8f3233311581ca68a99
Andreas <dev@appstractive.com>
2024-08-19 13:52:08 +0200

    Add OptIn

--- jwt-jwks/src/commonMain/kotlin/com/appstractive/jwt/Common.kt
+++ jwt-jwks/src/commonMain/kotlin/com/appstractive/jwt/Common.kt
@@ -2,6 +2,7 @@
 
 import kotlinx.serialization.json.Json
 
+@OptIn(kotlinx.serialization.ExperimentalSerializationApi::class)
 internal val json: Json = Json {
   isLenient = true
   explicitNulls = false
