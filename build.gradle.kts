045c99bf6f1ad584320a9889f8a5c216b876bf64
Andreas Schulz <andr.schulz90@gmail.com>
2025-04-10 14:03:20 +0200

    Downgrade Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.appstractive"
 
-version = "1.1.0"
+version = "1.1.0-2.0.21"
 
 val localProps = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
