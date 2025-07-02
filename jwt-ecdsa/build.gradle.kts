65f17660747b6645abf63dd7e364fa658e95fa5f
Andreas Schulz <andr.schulz90@gmail.com>
2025-07-02 12:26:45 +0200

    Missing Publish Type

--- jwt-ecdsa/build.gradle.kts
+++ jwt-ecdsa/build.gradle.kts
@@ -1,6 +1,4 @@
-﻿@file:OptIn(ExperimentalWasmDsl::class)
-
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+﻿import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -16,7 +14,7 @@ version = rootProject.version
 
 kotlin {
   androidTarget {
-    publishLibraryVariants("release")
+    publishLibraryVariants("release", "debug")
     compilerOptions { jvmTarget.set(JvmTarget.JVM_17) }
   }
 
